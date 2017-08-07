8b7d5a88eee32bca45423d9c1f8a9469d005bd94
Antônio Jeremias <a.jeremiass@gmail.com>
2017-08-07 22:58:53 +0000

    Retornando user_id na autenticação

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -5,7 +5,9 @@ class AuthenticateUser
     @password = password
   end
   def call
-    JsonWebToken.encode(user_id: user.id) if user
+    if user
+      result = [JsonWebToken.encode(user_id: user.id), user.id]
+    end
   end
   private
   attr_accessor :email, :password
